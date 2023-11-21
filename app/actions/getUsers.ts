5f0b9d1f38f3a479c664c6f429c928f5c7304b19
이승준 <hhee200456@gmail.com>
2023-11-21 10:47:00 +0900

    배포 맛보기

--- app/actions/getUsers.ts
+++ app/actions/getUsers.ts
@@ -8,6 +8,7 @@ const getUsers = async () => {
   if (!session?.user?.email) {
     return [];
   }
+
   try {
     const users = await prisma.user.findMany({
       orderBy: {
@@ -20,8 +21,23 @@ const getUsers = async () => {
       },
     });
 
-    return users;
+    // Prisma에서 가져온 사용자 데이터를 일치하는 형식으로 매핑
+    const mappedUsers = users.map((user) => ({
+      id: user.id,
+      name: user.name ?? null,
+      email: user.email ?? null,
+      emailVerified: user.emailVerified ?? null,
+      image: user.image ?? null,
+      hashedPassword: user.hashedPassword ?? null,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+      conversationIds: user.conversationIds ?? [],
+      seenMessageIds: user.seenMessageIds ?? [],
+    }));
+
+    return mappedUsers;
   } catch (error: any) {
+    console.error("Error fetching users:", error);
     return [];
   }
 };
