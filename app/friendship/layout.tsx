9ffd7964398f9e897128441f9c19966cf83834f5
이승준 <hhee200456@gmail.com>
2023-11-21 11:03:54 +0900

    배포 오류 수정 1

--- app/friendship/layout.tsx
+++ app/friendship/layout.tsx
@@ -2,13 +2,11 @@ import getFriendships from "../actions/getFriendship";
 import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
 import FriendshipList from "./components/FriendshipList";
-import { User } from "@prisma/client";
 
 export default async function FriendshipLayout({
   children,
 }: {
   children: React.ReactNode;
-  users: User[];
 }) {
   const friendship = await getFriendships();
   const users = await getUsers();
